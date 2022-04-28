0cb8114d2d07a90950c3f5998f89d60d3a8c6453
guilhermeferreira7 <guilhermefribas@outlook.com>
2022-04-28 14:15:12 -0300

    collection page working, listing fake data

--- Readme.md
+++ Readme.md
@@ -10,14 +10,29 @@
 - Página com ranking
 
 ## Protótipos
-<img src="assets/docs/login.jpg" height="400"/>
-<img src="assets/docs/criar-conta.jpg" height="400"/>
-<img src="assets/docs/pagina-inicial.jpg" height="400"/>
-<img src="assets/docs/colecao.jpg" height="400"/>
-<img src="assets/docs/perfil.jpg" height="400"/>
-<img src="assets/docs/loja.jpg" height="400"/>
-<img src="assets/docs/ranking.jpg" height="400"/>
-<img src="assets/docs/interface-jogo.jpg" height="400"/>
+### Página de login
+<img src="docs/login.jpg" height="400"/>
+
+### Página de cadastro
+<img src="docs/criar-conta.jpg" height="400"/>
+
+### Página inicial
+<img src="docs/pagina-inicial.jpg" height="400"/>
+
+### Listagem da coleção
+<img src="docs/colecao.jpg" height="400"/>
+
+### Informações do perfil
+<img src="docs/perfil.jpg" height="400"/>
+
+### Loja
+<img src="docs/loja.jpg" height="400"/>
+
+### Ranking
+<img src="docs/ranking.jpg" height="400"/>
+
+### Interface de jogo
+<img src="docs/interface-jogo.jpg" height="400"/>
 
 ## Cronograma
-<img src="assets/docs/Cronograma.png" height="400"/>
+<img src="docs/Cronograma.png" height="400"/>
--- src/pages/Collection/index.jsx
+++ src/pages/Collection/index.jsx
@@ -1,24 +1,84 @@
-import { Text, View, FlatList, TouchableOpacity, Image } from 'react-native'
+import { 
+  View, 
+  FlatList, 
+  Image, 
+  Text,
+  StyleSheet,
+  Button
+} from 'react-native'
+// import axios from 'axios'
+import img1 from './1.png';
+import img2 from './2.png';
+import img3 from './3.png';
+import img4 from './4.png';
+
+import Header from "../../components/Header"
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Collection({ navigation }) {
-  const apiData = []
+  // https://neko-atsume.emshea.com/
+  // const fetch = () => { //axios.get('https://api.neko-atsume.emshea.com/cats')}
+  const apiData = [
+    {id: 5, img: img1, name: 'Snowball', power: '80', type: 'Comum'},
+    {id: 7, img: img2, name: 'Sapphire', power: '20', type: 'Raro'},
+    {id: 9, img: img3, name: 'Tabitha', power: '40', type: 'Comum'},
+    {id: 11, img: img4, name: 'Maple', power: '99', type: 'Comum'},
+  ]
 
   const Card = ({ item }) => {
     return(
-      <View>
-        <Image />
+      <View style={styles.card}>
+        <View style={styles.imageCard}>
+          <Image source={item.img} style={styles.imageSize}/>
+        </View>
+
+        <View style={styles.textInfo}>
+          <Text>Nome: {item.name}</Text>
+          <Text>Poder: {item.power}</Text>
+          <Text>Raridade: {item.type}</Text>
+        </View>
+
+        <View style={styles.moreInfoBtn}>
+          <Button title='Mais informações' />
+        </View>
       </View>
     );
   };
 
   return (
     <View>
-      <FlatList
-        data={apiData}
-        renderItem={Card}
-        keyExtractor={(item) => item.id}
-      />
-
+      <Header />
+      <SafeAreaView>
+        <FlatList
+          style={styles.collectionList}
+          data={apiData}
+          renderItem={Card}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  collectionList: {
+    padding: 20,
+  },
+  card: {
+    flexDirection: 'row',
+  },
+  textInfo: {
+    flex: 2,
+  },
+  imageCard: {
+    flex: 2,
+    marginBottom: 20
+  },
+  imageSize: {
+    width: 114,
+    height: 80,
+  },
+  moreInfoBtn: {
+    flex: 2,
+  }
+});
