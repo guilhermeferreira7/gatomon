23177f1b31705d9b29d47f86406d84127fe14f8c
guilhermeferreira7 <guilhermefribas@outlook.com>
2022-05-18 20:52:52 -0300

    upd

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -8,7 +8,6 @@ import Login from "./pages/Login";
 import Ranking from "./pages/Ranking";
 import Store from "./pages/Store";
 import Collection from "./pages/Collection";
-import Header from "./components/Header";
 import { Button } from "react-native";
 
 const Stack = createNativeStackNavigator();
@@ -18,10 +17,36 @@ export default function Routes() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Account" component={Account} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerRight: () => {
+              return <Button onPress={() => {}} title={"logout"}></Button>;
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Account"
+          component={Account}
+          options={{
+            title: "Conta",
+            headerRight: () => {
+              return <Button onPress={() => {}} title={"logout"}></Button>;
+            },
+          }}
+        />
         <Stack.Screen name="CreateAccount" component={CreateAccount} />
-        <Stack.Screen name="Game" component={Game} />
+        <Stack.Screen
+          name="Game"
+          component={Game}
+          options={{
+            title: "Jogar",
+            headerRight: () => {
+              return <Button onPress={() => {}} title={"logout"}></Button>;
+            },
+          }}
+        />
         <Stack.Screen
           name="Collection"
           component={Collection}
@@ -32,9 +57,25 @@ export default function Routes() {
             },
           }}
         />
-        <Stack.Screen name="Store" component={Store} />
-        <Stack.Screen name="Ranking" component={Ranking} />
-        <Stack.Screen name="Header" component={Header} />
+        <Stack.Screen
+          name="Store"
+          component={Store}
+          options={{
+            title: "Loja",
+            headerRight: () => {
+              return <Button onPress={() => {}} title={"logout"}></Button>;
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Ranking"
+          component={Ranking}
+          options={{
+            headerRight: () => {
+              return <Button onPress={() => {}} title={"logout"}></Button>;
+            },
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
